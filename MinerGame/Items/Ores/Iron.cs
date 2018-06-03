861311ffb5ba05eca583693d18eafa102c637c6a
Thomas Micol <thomasmicol@gmail.com>
2018-06-03 23:22:20 +1200

    Added ore items

---
+++ MinerGame/Items/Ores/Iron.cs
@@ -0,0 +1,21 @@
+﻿using MinerGame.World.Tiles.Ores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerGame.World.Tiles
+{
+    class Iron : Item
+    {
+        public Iron()
+        {
+            Name = "Iron";
+            Description = "Good ol' iron. You will be needing a lot of this";
+            Value = 7;
+            Weight = 6;
+        }
+        
+    }
+}
---
+++ MinerGame/Items/Ores/Silver.cs
@@ -0,0 +1,20 @@
+﻿using MinerGame.World.Tiles.Ores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerGame.World.Tiles
+{
+    class Silver : Item
+    { 
+        public Silver()
+        {
+            Name = "Silver";
+            Description = "Half as shiney as gold; half as heavy too";
+            Value = 10;
+            Weight = 5;
+        }
+    }
+}

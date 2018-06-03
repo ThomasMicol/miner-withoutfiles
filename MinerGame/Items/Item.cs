7f05111f66ffc78e728058523530bc8bb882a424
Thomas Micol <thomasmicol@gmail.com>
2018-06-03 23:22:42 +1200

    Added the Item base class

---
+++ MinerGame/Items/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerGame
+{
+    public class Item : Sprite
+    {
+        protected string Name;
+        protected string Description;
+        protected string Rarity;
+        protected int Value;
+        protected int Weight;
+        protected int Count;
+        protected int StackSize;
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetName() { return Name; }
+    }
+}

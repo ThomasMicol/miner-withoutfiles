8e1376ebb2b504348f200f57d58519a88e8865e2
Thomas Micol <thomasmicol@gmail.com>
2018-06-03 21:59:50 +1200

    Added the basic ore classes and their base class mineable

--- MinerGame/Item.cs
+++ MinerGame/Item.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MinerGame
 {
-    public class Item
+    public class Item : Sprite
     {
         protected string Name;
         protected string Description;
---
+++ MinerGame/World/Tiles/Ores/Coal.cs
@@ -0,0 +1,13 @@
+﻿using MinerGame.World.Tiles.Ores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerGame.World.Tiles
+{
+    class Coal : Mineable
+    {
+    }
+}
